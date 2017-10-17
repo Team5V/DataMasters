50c5681d20e8b1b0ef85ff45f2986bac5682f6e3
Georgi Angelov <georgi.n.angelov@gmail.com>
2017-10-17 13:13:31 +0300

    Added Unit tests for Factory Classes

---
+++ BookStore/BookStore.UnitTests/Core/Factories/BookStoreFactoryTests/CreateBook_Should.cs
@@ -0,0 +1,26 @@
+﻿using BookStore.Core.Contracts;
+using BookStore.Core.Factories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.UnitTests.Core.Factories.BookStoreFactoryTests
+{
+    [TestClass]
+    public class CreateBook_Should
+    {
+        [TestMethod]
+        public void CreateBookObject_WhenParametersAreCorrect()
+        {
+            // Arrange 
+            
+            // Act
+            
+            // Assert
+        }
+    }
+}
---
+++ BookStore/BookStore.UnitTests/Core/Factories/CommandFactoryTests/Constructor_Should.cs
@@ -0,0 +1,32 @@
+﻿using BookStore.Core.Factories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Ninject;
+using System;
+
+namespace BookStore.UnitTests.Core.Factories.CommandFactoryTests
+{
+    [TestClass]
+    public class Constructor_Should
+    {
+        [TestMethod]
+        public void ThrowNull_WhenKernelIsNull()
+        {
+            // Arrange & Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new CommandFactory(null));
+        }
+
+        [TestMethod]
+        public void ReturnSuccess_WhenParametersAreCorrect()
+        {
+            //Arrange
+            var kernelMock = new Mock<IKernel>();
+
+            //Act
+            var commandFactory = new CommandFactory(kernelMock.Object);
+
+            //Assert
+            Assert.IsNotNull(commandFactory);
+        }
+    }
+}
