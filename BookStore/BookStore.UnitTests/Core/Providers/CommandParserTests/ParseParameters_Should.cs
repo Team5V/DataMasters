0bac98d81587a431a9229d35ea229e6a47c32971
Georgi Angelov <georgi.n.angelov@gmail.com>
2017-10-17 13:22:18 +0300

    deleted old code

--- BookStore/BookStore.UnitTests/Core/Providers/CommandParserTests/ParseParameters_Should.cs
+++ BookStore/BookStore.UnitTests/Core/Providers/CommandParserTests/ParseParameters_Should.cs
@@ -1,6 +1,5 @@
 ﻿using BookStore.Core.Factories;
 using BookStore.Core.Providers;
-using BookStore.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System.Collections.Generic;
@@ -12,26 +11,19 @@ namespace BookStore.UnitTests.Core.Providers.CommandParserTests
     public class ParseParameters_Should
     {
         [TestMethod]
-        public void ListWithCorrectParameters_WhenFullCommandContainsParameters()
+        public void ReturnListWithCorrectParameters_WhenFullCommandContainsParameters()
         {
             // Arrange
             var factoryMock = new Mock<ICommandFactory>();
 
             string fullCommand = "bookcreate:Nai-qkata Kniga;BG;80;Author1,Author2,Author3;Comedy";
 
-            //var authors = new List<Author>()
-            //{
-            //    new Author() { FullName = "Author1" },
-            //    new Author() { FullName = "Author2" },
-            //    new Author() { FullName = "Author3" }
-            //};
-
             List<string> expectedResult = new List<string>()
             {
                 "Nai-qkata Kniga",
                 "BG",
                 "80",
-                "Author1,Author2,Author3", // TODO: Fix - This should be a List(ICollection)
+                "Author1,Author2,Author3", 
                 "Comedy"
             };
 
