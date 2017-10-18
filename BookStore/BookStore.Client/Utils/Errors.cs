e563edd6f2526b71cb87f533a94cd9a9c4b39e53
ManchevCoding <manchevcoding@gmail.com>
2017-10-18 09:52:25 +0300

    Added Errors

---
+++ BookStore/BookStore.Client/Utils/Errors.cs
@@ -0,0 +1,14 @@
+﻿namespace BookStore.Client.Utils
+{
+    public class Err
+    {
+        public const string Params = "Invalid parameters.";
+        public const string Less = "Not enough parameters.";
+        public const string NoID = "The entry wasn`t found.";
+        public const string Exist = "Entry already exist.";
+        public const string Prop = "Invalid property.";
+        public const string Copy = "No copies left.";
+        public const string Factory = "Factory was not provided.";
+        public const string Context = "Context was not provided.";
+    }
+}
