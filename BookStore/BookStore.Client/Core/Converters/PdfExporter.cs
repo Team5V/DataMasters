bf0f500ff62c1fe754dca39c2e9fa89e9eb53bed
georgievlb <georgiev.lb@gmail.com>
2017-10-19 09:32:57 +0300

    Refactored PdfExporter

--- BookStore/BookStore.Client/Core/Converters/PdfExporter.cs
+++ BookStore/BookStore.Client/Core/Converters/PdfExporter.cs
@@ -2,6 +2,7 @@
 using iTextSharp.text;
 using iTextSharp.text.pdf;
 using System.Collections;
+using System.Data.Entity;
 using System.IO;
 using System.Linq;
 
@@ -9,6 +10,8 @@ namespace BookStore.Client.Core
 {
     public class PdfExporter : IPdfExporter
     {
+        private readonly string delimiter = "***************\n";
+
         public void ExportToPdf(IBookStoreContext context)
         {
             Document doc = new Document(PageSize.LETTER, 10, 10, 42, 35);
@@ -18,24 +21,28 @@ namespace BookStore.Client.Core
             doc.Open();
 
             Paragraph paragraph = new Paragraph();
-            //TODO refactor using lambda expressions syntax
-            var query = from book in context.Books
-                        from author in book.Authors
-                        select new
-                        {
-                            Title = book.Title,
-                            Authors = author.FullName,
-                            Language = book.Language,
-                            Pages = book.Pages,
-                            Genre = book.Genre
-                        } into x
-                        select x;
+
+            var query = context.Books.Include(x => x.Authors.Select(name => name.FullName))
+                .Select(book => new
+                {
+                    Title = book.Title,
+                    Authors = book.Authors.Select(a => a.FullName),
+                    Language = book.Language,
+                    Pages = book.Pages,
+                    Genre = book.Genre
+                });
 
             var queryToString = query.ToList();
 
-            foreach (var item in queryToString as IEnumerable)
+            foreach (var item in queryToString)
             {
-                paragraph.Add($"{item.ToString()}\n");
+                paragraph.Add(
+                    $"Title: {item.Title}\n" +
+                    $"Author(s): {string.Join(",", item.Authors)}\n" +
+                    $"Language: {item.Language}\n" +
+                    $"Genre: {item.Genre}\n" +
+                    $"Pages: {item.Pages}\n" +
+                    $"{this.delimiter}");
             }
 
             doc.Add(paragraph);
