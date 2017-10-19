375030d718676c9e20217026174046c22140f578
georgievlb <georgiev.lb@gmail.com>
2017-10-19 09:27:57 +0300

    Added BookDeleteCommand constructor tests

---
+++ BookStore/BookStore.UnitTests/Commands/BookCommands/BookDeleteCommandTests/Constructor_Should.cs
@@ -0,0 +1,35 @@
+﻿using BookStore.Client.Commands;
+using BookStore.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace BookStore.UnitTests.Commands.BookDeleteCommandTests
+{
+    [TestClass]
+    public class Constructor_Should
+    {
+        [TestMethod]
+        public void CreateInstance_WhenParametersAreCorrect()
+        {
+            //Arrange
+            var databaseMock = new Mock<IBookStoreContext>();
+
+            // Act
+            var command = new BookDeleteCommand(databaseMock.Object);
+
+            // Assert
+            Assert.IsNotNull(command);
+        }
+
+        [TestMethod]
+        public void ThrowException_WhenDatabaseIsNull()
+        {
+            // Arrange
+            var databaseMock = new Mock<IBookStoreContext>();
+
+            // Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new BookDeleteCommand(null));
+        }
+    }
+}
