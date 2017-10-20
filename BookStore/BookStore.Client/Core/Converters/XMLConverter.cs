9715db1afe805ef9b98e27e5006cf9a4efcc815a
ManchevCoding <manchevcoding@gmail.com>
2017-10-20 12:34:58 +0300

    Xml Converter

--- BookStore/BookStore.Client/Core/Converters/XMLConverter.cs
+++ BookStore/BookStore.Client/Core/Converters/XMLConverter.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Xml;
 
 namespace BookStore.Client.Core
 {
     internal class XMLConverter
     {
-        public string ReadLine()
+        private IEnumerable<Author> ReadAuthors(string documentUrl, string nodeUrl)
         {
-            throw new NotImplementedException();
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(documentUrl);
+            XmlNodeList nodeList = xmlDoc.DocumentElement.SelectNodes(nodeUrl);
+
+            var authors = new HashSet<Author>();
+            foreach (XmlNode node in nodeList)
+            {
+                var current = new Author();
+                current.Id = int.Parse(node.SelectSingleNode("Author_Id").InnerText);
+                current.FullName = node.SelectSingleNode("FullName").InnerText;
+                current.Bio = node.SelectSingleNode("Bio").InnerText;
+                authors.Add(current);
+            }
+
+            return authors;
         }
     }
 }
